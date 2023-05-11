15cfc545a55f71c452bb5e058ea4e809c772e1d7
Youkyeong <75975946+youkyeong60@users.noreply.github.com>
2023-05-12 00:03:11 +0900

    Fix: DMlist

--- pages/DirectMessage/index.tsx
+++ pages/DirectMessage/index.tsx
@@ -1,7 +1,56 @@
-import Workspace from "@layouts/Workspace";
-import React from "react";
+import React, { useCallback, useEffect, useRef } from 'react';
+import gravatar from 'gravatar';
+import useSWR from 'swr';
+import { Container, Header } from './style';
+import fetcher from '@utils/fetcher';
+import { useParams } from 'react-router';
+import ChatBox from '@components/ChatBox';
+import useInput from '@hooks/useInput';
+import axios from 'axios';
+import { IChat, IDM } from '@typings/db';
+import Scrollbars from 'react-custom-scrollbars-2';
+import ChatList from '@components/ChatList';
+import makeSection from '@utils/makeSection';
 
 const DirectMessage = () => {
-  return <div>dm</div>;
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR(`/api/users`, fetcher);
+  const [chat, onChangeChat, setChat] = useInput('');
+  const {
+    data: chatData,
+    mutate: mutateChat,
+    mutate,
+  } = useSWR<IDM[]>(`/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`, fetcher);
+  const onSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log(chat);
+      if (chat?.trim()) {
+        axios
+          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+            content: chat,
+          })
+          .then(() => {
+            mutate();
+            setChat('');
+          })
+          .catch(console.error);
+      }
+    },
+    [chat],
+  );
+
+  if (!userData || !myData) return null;
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nicknam} />
+        <span>{userData.nickname}</span>
+      </Header>
+      <ChatList chatData={chatData} />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
+    </Container>
+  );
 };
 export default DirectMessage;
